4a5631deb9b3aaab27554305381fbf4d0166e14c
user <user@MacBook-Pro-user.local>
2022-10-05 10:40:11 +0300

    add ex_47

---
+++ ex_47/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void Array(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(new Random().Next(-10, 10) + new Random().NextDouble(), 1);
+        }
+    }
+}
+
+void Print(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Строк в массиве: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Столбцов в массиве: ");
+int n = int.Parse(Console.ReadLine());
+
+double[,] mass = new double[m, n];
+
+Array(mass);
+Print(mass);
