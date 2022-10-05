459aaa1e6e5974ab4e402c9d0861e12cb0083d24
user <user@MacBook-Pro-user.local>
2022-10-05 10:50:29 +0300

    add ex_50

---
+++ ex_50/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+void Array(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void Print(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] mass = new int[8, 8];
+
+Array(mass);
+Print(mass);
+Console.WriteLine();
+
+Console.Write("Введите позицию: ");
+int position = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+if (position > mass.Length) Console.WriteLine("Такого элемента нет");
+else
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            if (count == position) Console.WriteLine($"Значение элемента на позиции {position} = {mass[i, j]}");
+            count++;
+        }
+    }
+}
